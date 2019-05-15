aceb76c9a46f776f5766caa5a50be2328c25e5f4
izzysilv <35540604+izzysilv@users.noreply.github.com>
2019-05-14 20:54:18 -0600

    complete contacts

--- cms/src/app/contacts/contacts-list/contacts-list.component.ts
+++ cms/src/app/contacts/contacts-list/contacts-list.component.ts
@@ -7,7 +7,7 @@ import { Contact } from '../contacts.model';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  // @Output() selectedContactEvent = new EventEmitter<Contact>();
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [
     new Contact('1' , 'Bro. Jackson', 'jacksonk@byui,edu', '208-496-3771', 'http://web.byui.edu/Directory/Employee/jacksonk.jpg', 'null'),
     new Contact('2', 'Bro. Barzee', 'barzeer@byui.edu', '208-496-3768', 'http://web.byui.edu/Directory/Employee/barzeer.jpg', 'null')
@@ -19,8 +19,8 @@ export class ContactsListComponent implements OnInit {
   }
 
   onSelected(contact: Contact) {
-    // this.selectedContactEvent.emit(contact);
-    this.contacts.push(contact);
+    console.log(contact);
+    this.selectedContactEvent.emit(contact);
   }
 
 }
